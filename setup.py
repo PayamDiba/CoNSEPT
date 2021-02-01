5479eb000e7179a08004f1b93844dd1bfee2302f
Payam Dibaeinia <p.dibaeinia@gmail.com>
2021-02-01 14:18:32 -0600

    updated

---
+++ setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.2.0'
+PACKAGE_NAME = 'CoNSEPT'
+AUTHOR = 'Payam Dibaeinia'
+AUTHOR_EMAIL = 'dibaein2@illinois.edu'
+URL = 'https://github.com/PayamDiba/CoNSEPT'
+
+LICENSE = 'MIT License'
+DESCRIPTION = 'A Convolutional Neural Network-based Sequence-to-Expression Prediction Tool'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'tensorflow>=2.3',
+      'numpy',
+      'pandas',
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      entry_points ={'console_scripts': [ 'consept = CoNSEPT.train:main']},
+      python_requires='>3.5.2',
+      )
